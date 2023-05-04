ffcb81feb489d7d528f96f2bbcd783be5cd85bc4
Adriano Maurmann <adriano.maurmann@gmail.com>
2023-05-03 21:58:24 -0300

    remove old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-  title = 'sample-app';
-  opened: boolean = true;
+  sideBarOpened: boolean = true;
 
-  messages: string[] = [];
-
-  ngOnInit(): void {
-    this.messages.push('adriano');
-    this.messages.push('lucio');
-    this.messages.push('rebeschini');
-    this.messages.push('maurmann');
-  }
+  ngOnInit(): void {}
 
   toggleMenu(): void {
-    this.opened = !this.opened;
+    this.sideBarOpened = !this.sideBarOpened;
   }
 }
